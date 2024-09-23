e5977db1bbe314734295b9ca4089b1649fe82f90
Octavian Duiu <oduiu@suvoda.com>
2024-09-24 02:20:54 +0300

    Fixed form submission

--- submit-form.php
+++ submit-form.php
@@ -1,22 +1,30 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $name = htmlspecialchars($_POST['name']);
     $company = htmlspecialchars($_POST['companie'] ?? '');    
     $phone = htmlspecialchars($_POST['telefon']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to = 'office.priorityguard@gmail.com'; 
+    $headers = 'From: no-reply@priorityguard.com' . "\r\n" .
+           'Reply-To: no-reply@priorityguard.com' . "\r\n" .
+           'X-Mailer: PHP/' . phpversion();
+    $to = 'contact@priorityguard.ro'; 
+    $subject = 'Solicitare ofertă';
 
-    $subject = 'Solicitare oferta';
-
-    $email_content = "Name: $name\n";
-    $email_content .= "Company: $company\n";
-    $email_content .= "Phone: $phone\n";
+    $email_content = "Nume: $name\n";
+    $email_content .= "Companie: $company\n";
+    $email_content .= "Telefon: $phone\n";
     $email_content .= "Email: $email\n";
-    $email_content .= "Message:\n$message\n";
+    $email_content .= "Mesaj:\n$message\n";
 
-    mail($to, $subject, $email_content);
+    if(mail($to, $subject, $email_content, $headers)) {
+        echo "Mesaj trimis cu succes!";
+    } else {
+        echo "Eroare la trimiterea mesajului.";
+    }
+    
     exit;
 }
 ?>
