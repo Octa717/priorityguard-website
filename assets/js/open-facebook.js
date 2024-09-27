9d83c264a7073a6fccddaafcc4b2be9a7ad62b3a
Octavian Duiu <oduiu@suvoda.com>
2024-09-28 00:32:10 +0300

    Fixed facebook web link

--- assets/js/open-facebook.js
+++ assets/js/open-facebook.js
@@ -2,7 +2,7 @@ function openFacebook() {
     var username = 'Priority-Guard-Security';
     var usercode = '61566478231557/';
     var appUrl = 'fb://profile/' + usercode; 
-    var webUrl = 'https://www.facebook.com/people/' + username + usercode;
+    var webUrl = 'https://www.facebook.com/people/' + username + '/' + usercode;
 
     // Check if the user is on a mobile device
     if (/Mobi|Android/i.test(navigator.userAgent)) {
